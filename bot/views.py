ceff7ca569e9f384267aaa7f73dd5a897c079144
unknown <maybetomorrow789@gmail.com>
2023-02-21 18:39:07 +0800

    大樂透函式

--- bot/views.py
+++ bot/views.py
@@ -9,6 +9,10 @@ from linebot import LineBotApi, WebhookHandler, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage,TextMessage,ImageSendMessage
 import random
+from bs4 import BeautifulSoup
+import requests
+import numpy as np
+
 
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parse = WebhookParser(settings.LINE_CHANNEL_SECRET)
@@ -36,11 +40,12 @@ def callback(request):
                     message,image_url=None,None
                     text=event.message.text
                     print(text)
-                    if '電影' in text:
+                    if '大樂透'==text:
+                        message=get_biglotto()
+                    elif '電影' in text:
                         message='https://movies.yahoo.com.tw/'
                     elif '台中捷運' in text:
                         image_url='https://upload.wikimedia.org/wikipedia/commons/c/c0/Taichung_MRT_Planning_Route_Map.jpg'
-                        
                     elif '台北捷運' in text:
                         message='https://www.travel.taipei/Content/images/static/travel-tips/metrotaipeimap.jpg'
                     elif '早安' in text:
@@ -68,3 +73,20 @@ def lotto():
     result = ' '.join(map(str, numbers))
     n = random.randint(1, 50)
     return f'{result} 特別號:{n}'
+
+
+
+def get_biglotto():
+    url='https://www.taiwanlottery.com.tw/lotto/Lotto649/history.aspx'
+    resp=requests.get(url)
+    soup=BeautifulSoup(resp.text,'lxml')
+    trs=soup.find('table',class_='table_org td_hm').find_all('tr')
+    numbers=[td.text.strip() for td in trs[4].find_all('td')[1:]]
+    numbers=' '.join(numbers[:-1])+'特別號'+numbers[-1]
+    data1=[td.text.strip() for td in trs[0].find_all('td')]
+    data2=[td.text.strip() for td in trs[1].find_all('td')]
+    data=list(zip(data1,data2))
+    title=':'.join(np.array(data).reshape(10))
+    result=f'{title}\n{numbers}'
+    
+    return result
